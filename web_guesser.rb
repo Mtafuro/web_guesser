436d463fd3b8ceb50bcb29a1a5eccf1637961996
mtafuro <tafuro@outlook.com>
2017-05-09 09:54:09 -0500

    Number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,20 @@ require 'sinatra/reloader'
 rng = rand(100)
 
 get '/' do
-  erb :index, :locals => {:rng => rng}
+  guess = params["guess"].to_i
+    case
+      when (guess - rng) > 5
+        msg = "WAY too high!"
+      when (guess - rng) < (-5)
+        msg = "WAY too low"
+      when guess > rng
+        msg = "a bit too high!"
+      when guess < rng
+        msg = "a bit too low!"
+      when guess == rng
+        msg = "is DEAD ON. The secret number is #{rng}."
+    end
+
+  erb :index, :locals => {:rng => rng, :msg => msg, :guess => guess}
+
 end
