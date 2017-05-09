9c79d0ef283d6af778f7055b91e946a5da490ee9
mtafuro <tafuro@outlook.com>
2017-05-09 13:14:20 -0500

    added color functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,16 +8,26 @@ get '/' do
     case
       when (guess - rng) > 5
         msg = "WAY too high!"
+        css = "background: red;"
+        # RED
       when (guess - rng) < (-5)
         msg = "WAY too low"
+        css = "background: red;"
+        # RED
       when guess > rng
         msg = "a bit too high!"
+        css = "background: pink;"
+        # PINK
       when guess < rng
         msg = "a bit too low!"
+        css = "background: pink;"
+        # PINK
       when guess == rng
         msg = "is DEAD ON. The secret number is #{rng}."
+        css = "background: green;"
+        # GREEN
     end
 
-  erb :index, :locals => {:rng => rng, :msg => msg, :guess => guess}
+  erb :index, :locals => {:rng => rng, :msg => msg, :guess => guess, :css => css}
 
 end
